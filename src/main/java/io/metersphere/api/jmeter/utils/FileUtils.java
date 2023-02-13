71f42ec0b4d0c143033e99e6a9cfa8b23e73b484
fit2-zhao <yong.zhao@fit2cloud.com>
2023-02-13 10:19:46 +0800

    refactor(接口测试): 优化三方插件加载

--- src/main/java/io/metersphere/api/jmeter/utils/FileUtils.java
+++ src/main/java/io/metersphere/api/jmeter/utils/FileUtils.java
@@ -1,15 +1,7 @@
 package io.metersphere.api.jmeter.utils;
 
-import io.metersphere.api.service.utils.BodyFile;
 import io.metersphere.utils.LoggerUtil;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.jmeter.config.CSVDataSet;
-import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
-import org.apache.jmeter.protocol.http.util.HTTPFileArg;
-import org.apache.jorphan.collections.HashTree;
-import org.aspectj.util.FileUtil;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -28,55 +20,10 @@ public class FileUtils {
     public static final String KEYSTORE_FILE_PATH = "MS-KEYSTORE-FILE-PATH";
     public static final String BODY_PLUGIN_FILE_DIR = "/opt/metersphere/data/body/plugin/";
 
-    public static void createFiles(MultipartFile[] bodyFiles, String path) {
-        if (bodyFiles != null && bodyFiles.length > 0) {
-            File testDir = new File(path);
-            if (!testDir.exists()) {
-                testDir.mkdirs();
-            }
-            for (int i = 0; i < bodyFiles.length; i++) {
-                MultipartFile item = bodyFiles[i];
-                File file = new File(path + "/" + item.getOriginalFilename());
-                try (InputStream in = item.getInputStream(); OutputStream out = new FileOutputStream(file)) {
-                    file.createNewFile();
-                    FileUtil.copyStream(in, out);
-                } catch (IOException e) {
-                    LoggerUtil.error(e);
-                    MSException.throwException("文件处理异常");
-                }
-            }
-        }
-    }
-
     public static void deleteFile(String path) {
         File file = new File(path);
         if (file.exists()) {
             file.delete();
-        }
-    }
-
-    private static File[] getFiles(File dir) {
-        return dir.listFiles((f, name) -> {
-            File jar = new File(f, name);
-            return jar.isFile() && jar.canRead();
-        });
-    }
-
-
-    public static void deletePath(String path) {
-        File file = new File(path);
-        if (file != null && file.isDirectory()) {// $NON-NLS-1$
-            file = new File(path + "/");
-        }
-        if (file != null) {
-            File[] files = getFiles(file);
-            if (ArrayUtils.isNotEmpty(files)) {
-                for (int i = 0; i < files.length; i++) {
-                    if (files[i] != null && files[i].exists()) {
-                        files[i].delete();
-                    }
-                }
-            }
         }
     }
 
@@ -88,40 +35,6 @@ public class FileUtils {
             }
         } catch (Exception e) {
             LoggerUtil.error(e);
-        }
-    }
-
-    public static void getFiles(HashTree tree, List<BodyFile> files) {
-        for (Object key : tree.keySet()) {
-            HashTree node = tree.get(key);
-            if (key instanceof HTTPSamplerProxy) {
-                HTTPSamplerProxy source = (HTTPSamplerProxy) key;
-                if (source != null && source.getHTTPFiles().length > 0) {
-                    for (HTTPFileArg arg : source.getHTTPFiles()) {
-                        BodyFile file = new BodyFile();
-                        file.setId(arg.getParamName());
-                        file.setName(arg.getPath());
-                        if (arg.getPropertyAsBoolean(IS_REF)) {
-                            file.setRefResourceId(arg.getPropertyAsString(FILE_ID));
-                        }
-                        files.add(file);
-                    }
-                }
-            } else if (key instanceof CSVDataSet) {
-                CSVDataSet source = (CSVDataSet) key;
-                if (source != null && StringUtils.isNotEmpty(source.getPropertyAsString(FILENAME))) {
-                    BodyFile file = new BodyFile();
-                    file.setId(source.getPropertyAsString(FILENAME));
-                    file.setName(source.getPropertyAsString(FILENAME));
-                    if (source.getPropertyAsBoolean(IS_REF)) {
-                        file.setRefResourceId(source.getPropertyAsString(FILE_ID));
-                    }
-                    files.add(file);
-                }
-            }
-            if (node != null) {
-                getFiles(node, files);
-            }
         }
     }
 
